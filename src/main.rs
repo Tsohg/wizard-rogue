7bc4afc95c4bc6c196df16c47ffac4a36b845083
Luminight <balazscsabai98@gmail.com>
2024-06-23 00:11:59 +0200

    setup bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_systems(Startup, spawn_entities)
+        .add_systems(Update, print_player)
+        .run();
+}
+
+#[derive(Debug, Component)]
+struct Player;
+
+#[derive(Debug, Component)]
+struct Name(String);
+
+fn spawn_entities(mut commands: Commands) {
+    commands.spawn((Player, Name("Ali".to_string())));
+    commands.spawn(Name("Labrat".to_string()));
+    commands.spawn(Name("Lumi".to_string()));
+}
+
+fn print_player(query: Query<&Name, With<Player>>) {
+    for name in &query {
+        println!("hello {}!", name.0)
+    }
 }
